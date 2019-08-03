225d5a4d4982f78fdfde6ef8e1f5eed25ec19cfb
adinkins2019 <afdinki@gmail.com>
2019-08-02 21:59:57 -0400

    Modified marketing page files and added pictures

--- assets/js/aboutus.js
+++ assets/js/aboutus.js
@@ -1,17 +1,17 @@
 document.title = "About Us";
 
-const missionStatement = "";
-const visionStatement = "";
-const ourStory = "";
+const missionStatement = "The mission of The Bountiful Children's Foundation is to foster a culture and                                     environment that shares the communal values of love, peace, and hope and that we usher an                          era where men, women, and children are not confined to what that current have for their                            daily sustenance, that they have access to food that has nutritional value, and is                                 readily available to all that need it. We aim to make the global hunger crisis a thing of                          the past.";
+const visionStatement = "Our ultimate aim the end of malnutrition the worldwide hunger crisis, especially amongst                           those of its most vulnerable members, children. While most of our initial efforts have                             revovled around serving disparate populations in Central and South America, we plan on                             expanding our global footprint within the next five years and increase our global reach                            by increasing operations in Western, Southern, and Central Africa and also expanding                               operations in Eastern Europe and disparate areas of Asia and the Pacific Rim.";
+const ourStory = "Our organization began as a small outfit of concerned global citizens who bonded over a single                    cause  and want to make a difference in the lives of those born and raised under extreme                           circumstances and to effectively end childhood hunger and put a dent in the current global hunger                  crisis. Since our humble beginnings, we have expanded operations to have a global reach and a                      global impact on the lives of many, deploying services to the needy in 19 different countries                      across 4 continents";
+
 
 function modifyLastElementText(className, aStr){
-    let newID = document.querySelector(className);
+    let cName = '.' + className;
+    let newID = document.querySelector(cName);
     newID.lastElementChild.textContent = aStr;
 }
 
-function changeEmpPhoto(photoName){
-    let newPhoto = document.query
-}
+
 class Person {
     constructor(attrs) {
         this.fName = attrs.fName;
@@ -62,8 +62,8 @@ const empOne = new Employee ({
 const empTwo = new Employee ({
     fName: "Junior",
     lName: "Dugue",
-    title: "React Developer",
-    description: "Avid photographer, coder extraordinaire, current Lambda student (WebPT6)",
+    title: "Front End Engineer",
+    description: "Junior Dugue is our senior developer. He has an in depth understanding of HTML, CSS, Javascript, REACT, and other development platforms. He is also an avid photographer and animal lover.",
     country: "United States",
     location: "Florida",
     photo: "juniordugue.png"
